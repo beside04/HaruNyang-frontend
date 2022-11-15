cfcceb605a7b6d94f7a1c057d3188a648d30f8f6
Geon Woo Jeon <fghjjkl2700@gmail.com>
2022-11-15 15:20:49 +0900

    setting : 기본 환경 설정 및 컬러 변수 추가

---
+++ lib/config/theme/color_data.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+const kPrimaryColor1 = Color(0xff0DC9B8);
+const kPrimaryColor2 = Color(0xff5973FF);
+const kPrimaryColor3 = Color(0xffFBDB51);
+const kPrimaryColor4 = Color(0xffF6C944);
+
+const kFillGrayColor = Color(0xFFF8F8F8);
+const kBlackColor = Color(0xFF000000);
+const kPlaceHolderTextColor = Color(0xffBCBCBC);
