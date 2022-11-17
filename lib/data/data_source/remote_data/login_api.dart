52d356b88cef8d47b25b7d3675c3a9668e9833ec
JINWOO <njw9108@naver.com>
2022-11-17 23:51:44 +0900

    feat: Add Login Api

---
+++ lib/data/data_source/remote_data/login_api.dart
@@ -0,0 +1,30 @@
+import 'dart:convert';
+
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:http/http.dart' as http;
+
+class LoginApi {
+  String baseUrl = dotenv.env['API_BASE_URL'] ?? '';
+  final http.Client _client = http.Client();
+
+  Future<void> login(String loginType, String socialId) async {
+    String loginUrl = '$baseUrl/v1/login';
+    try {
+      http.Response response;
+      Uri uri = Uri.parse(loginUrl);
+      response = await _client.post(
+        uri,
+        body: {
+          'login_type': loginType,
+          'social_id': socialId,
+        },
+      );
+      if (response.statusCode != 200) {
+        throw Exception('create juso api의 응답 코드가 200이 아닙니다.');
+      }
+      print(response.body);
+      final json = jsonDecode(response.body);
+
+    } catch (e) {}
+  }
+}
