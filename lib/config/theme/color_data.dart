d5ac44610a83da553255fc6defcb6f850844cfc5
Geon Woo Jeon <fghjjkl2700@gmail.com>
2022-11-18 01:42:20 +0900

    add : Color, Font 데이터 추가

--- lib/config/theme/color_data.dart
+++ lib/config/theme/color_data.dart
@@ -1,10 +1,43 @@
 import 'package:flutter/material.dart';
 
+//Primary Color
 const kPrimaryColor1 = Color(0xff0DC9B8);
 const kPrimaryColor2 = Color(0xff5973FF);
 const kPrimaryColor3 = Color(0xffFBDB51);
 const kPrimaryColor4 = Color(0xffF6C944);
 
+const kSecondaryColor = Color(0xff3a3d47);
+const kErrorColor = Color(0xfff15a5b);
+const kSuccessColor = Color(0xff5973ff);
+
+//Light Mode
+const kBackGroundLightColor = Color(0xffffffff);
+const kSurfaceLightColor = Color(0xfff5f5f5);
+
+//Dark Mode
+const kBackGroundDarkColor = Color(0xff1b1b1e);
+const kSurfaceDarkColor = Color(0xff27282e);
+
+//Gray Scale Color
+const kBlackColor = Color(0xff111111);
+const kGrayColor950 = Color(0xff161618);
+const kGrayColor900 = Color(0xff1b1b1e);
+const kGrayColor850 = Color(0xff212227);
+const kGrayColor800 = Color(0xff27282e);
+const kGrayColor750 = Color(0xff2d2f37);
+const kGrayColor700 = Color(0xff363944);
+const kGrayColor650 = Color(0xff3a3d47);
+const kGrayColor600 = Color(0xff4c4f5d);
+const kGrayColor550 = Color(0xff585c6a);
+const kGrayColor500 = Color(0xff666a7a);
+const kGrayColor400 = Color(0xff848999);
+const kGrayColor300 = Color(0xffa5a8b4);
+const kGrayColor250 = Color(0xffb7bac3);
+const kGrayColor200 = Color(0xffcfd1d7);
+const kGrayColor150 = Color(0xffdcdde2);
+const kGrayColor100 = Color(0xffeaebed);
+const kGrayColor50 = Color(0xfff5f5f5);
+const kWhiteColor = Color(0xffffffff);
+
+//temp data
 const kFillGrayColor = Color(0xFFF8F8F8);
-const kBlackColor = Color(0xFF000000);
-const kPlaceHolderTextColor = Color(0xffBCBCBC);
