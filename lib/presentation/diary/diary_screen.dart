4cfaa5ba8d0598d41f7f432a61730e2c86a21326
Geon Woo Jeon <fghjjkl2700@gmail.com>
2022-12-03 18:01:15 +0900

    refactoring : 이미지 편집 기능 view model 연결

--- lib/presentation/diary/diary_screen.dart
+++ lib/presentation/diary/diary_screen.dart
@@ -1,25 +1,127 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:frontend/di/getx_binding_builder_call_back.dart';
 import 'package:frontend/presentation/diary/diary_view_model.dart';
 import 'package:get/get.dart';
 
 class DiaryScreen extends GetView<DiaryViewModel> {
-  const DiaryScreen({Key? key}) : super(key: key);
+  const DiaryScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
     getDiaryBinding();
+    final screenWidth = MediaQuery.of(context).size.width;
+    final screenHeight = MediaQuery.of(context).size.height;
+
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('다이어리'),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: const [
-            Text('다이어리'),
-          ],
-        ),
+      body: Column(
+        mainAxisSize: MainAxisSize.max,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Obx(
+            () => Expanded(
+              child: (controller.croppedFile.value != null ||
+                      controller.pickedFile.value != null)
+                  ? Center(
+                      child: Column(
+                        mainAxisSize: MainAxisSize.min,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          Padding(
+                            padding:
+                                const EdgeInsets.symmetric(horizontal: 16.0),
+                            child: Card(
+                              elevation: 4.0,
+                              child: Padding(
+                                padding: const EdgeInsets.all(16.0),
+                                child: controller.croppedFile.value != null
+                                    ? ConstrainedBox(
+                                        constraints: BoxConstraints(
+                                          maxWidth: 0.8 * screenWidth,
+                                          maxHeight: 0.7 * screenHeight,
+                                        ),
+                                        child: Image.file(
+                                          File(controller
+                                              .croppedFile.value!.path),
+                                        ),
+                                      )
+                                    : controller.pickedFile.value != null
+                                        ? ConstrainedBox(
+                                            constraints: BoxConstraints(
+                                              maxWidth: 0.8 * screenWidth,
+                                              maxHeight: 0.7 * screenHeight,
+                                            ),
+                                            child: Image.file(
+                                              File(controller
+                                                  .pickedFile.value!.path),
+                                            ),
+                                          )
+                                        : const SizedBox.shrink(),
+                              ),
+                            ),
+                          ),
+                          const SizedBox(height: 24.0),
+                          Row(
+                            mainAxisSize: MainAxisSize.min,
+                            children: [
+                              FloatingActionButton(
+                                onPressed: () {
+                                  controller.clear();
+                                },
+                                backgroundColor: Colors.redAccent,
+                                tooltip: 'Delete',
+                                child: const Icon(Icons.delete),
+                              ),
+                              if (controller.croppedFile.value == null)
+                                Padding(
+                                  padding: const EdgeInsets.only(left: 32.0),
+                                  child: FloatingActionButton(
+                                    onPressed: () {
+                                      controller.cropImage();
+                                    },
+                                    backgroundColor: const Color(0xFFBC764A),
+                                    tooltip: 'Crop',
+                                    child: const Icon(Icons.crop),
+                                  ),
+                                )
+                            ],
+                          ),
+                        ],
+                      ),
+                    )
+                  : Center(
+                      child: Card(
+                        elevation: 4.0,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(16.0),
+                        ),
+                        child: SizedBox(
+                          width: 320.0,
+                          height: 300.0,
+                          child: Column(
+                            mainAxisSize: MainAxisSize.max,
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Padding(
+                                padding:
+                                    const EdgeInsets.symmetric(vertical: 24.0),
+                                child: ElevatedButton(
+                                  onPressed: () {
+                                    controller.uploadImage();
+                                  },
+                                  child: const Text('Upload'),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
+            ),
+          ),
+        ],
       ),
     );
   }
--- lib/presentation/diary/diary_view_model.dart
+++ lib/presentation/diary/diary_view_model.dart
@@ -1,5 +1,8 @@
+import 'package:frontend/config/theme/color_data.dart';
 import 'package:frontend/domain/use_case/social_login_use_case/kakao_login_use_case.dart';
 import 'package:get/get.dart';
+import 'package:image_cropper/image_cropper.dart';
+import 'package:image_picker/image_picker.dart';
 
 class DiaryViewModel extends GetxController {
   final KakaoLoginUseCase kakaoLoginUseCase;
@@ -7,6 +10,47 @@ class DiaryViewModel extends GetxController {
   DiaryViewModel({
     required this.kakaoLoginUseCase,
   });
+
+  final pickedFile = Rx<XFile?>(null);
+  final croppedFile = Rx<CroppedFile?>(null);
+
+  Future<void> cropImage() async {
+    if (pickedFile.value != null) {
+      var croppedImage = await ImageCropper().cropImage(
+        sourcePath: pickedFile.value!.path,
+        compressFormat: ImageCompressFormat.jpg,
+        compressQuality: 100,
+        uiSettings: [
+          AndroidUiSettings(
+            toolbarTitle: 'Cropper',
+            toolbarColor: kPrimaryColor,
+            toolbarWidgetColor: kWhiteColor,
+            initAspectRatio: CropAspectRatioPreset.original,
+            lockAspectRatio: false,
+          ),
+          IOSUiSettings(
+            title: 'Cropper',
+          ),
+        ],
+      );
+      if (croppedImage != null) {
+        croppedFile.value = croppedImage;
+      }
+    }
+  }
+
+  Future<void> uploadImage() async {
+    final pickedImage =
+        await ImagePicker().pickImage(source: ImageSource.gallery);
+    if (pickedImage != null) {
+      pickedFile.value = pickedImage;
+    }
+  }
+
+  void clear() {
+    pickedFile.value = null;
+    croppedFile.value = null;
+  }
 
   Future<void> logout() async {
     await kakaoLoginUseCase.logout();
