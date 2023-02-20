013cdef1dc7604b804b4a74b6a7161597dfc0d79
Geon Woo Jeon <fghjjkl2700@gmail.com>
2023-02-20 11:59:54 +0900

    hotfix : 북마크 표시 isbookmark 변경

--- lib/presentation/profile/book_mark/book_mark_screen.dart
+++ lib/presentation/profile/book_mark/book_mark_screen.dart
@@ -87,8 +87,9 @@ class _BookMarkScreenState extends State<BookMarkScreen> {
                       )
                     : Obx(
                         () => BookMarkList(
-                          date: DateTime.parse(diaryController.state.value.bookmarkList[index].createdAt),
-                          isBookMark: false,
+                          date: DateTime.parse(diaryController
+                              .state.value.bookmarkList[index].createdAt),
+                          isBookMark: true,
                           title: diaryController.state.value.bookmarkList[index]
                               .wiseSaying.message,
                           name: diaryController.state.value.bookmarkList[index]
