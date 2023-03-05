644cb31703d874f750c83f7ecb9f57ee92749334
Geon Woo Jeon <fghjjkl2700@gmail.com>
2023-03-05 17:51:12 +0900

    hotfix : widget 라이프 사이클 버그 수정

--- lib/presentation/home/home_screen.dart
+++ lib/presentation/home/home_screen.dart
@@ -17,9 +17,11 @@ class HomeScreen extends GetView<HomeViewModel> {
   @override
   Widget build(BuildContext context) {
     //처음 가입한 유저라면 일기쓰기 화면으로 이동
-    Get.arguments == null
-        ? controller.selectedIndex.value = 0
-        : controller.selectedIndex.value = Get.arguments['index'];
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      Get.arguments == null
+          ? controller.selectedIndex.value = 0
+          : controller.selectedIndex.value = Get.arguments['index'];
+    });
 
     final mainViewController = Get.find<MainViewModel>();
 
